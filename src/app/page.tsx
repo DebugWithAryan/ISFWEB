f1ae5c800a72381bcd83b0d571252466990db09b
DebugWithAryan <aryanjaiswal123123@gmail.com>
2025-08-24 19:37:34 +0530

    First commit for ISFWEB project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -136,8 +136,7 @@ const animatedText = {
   transition: { duration: 0.4 },
 };
 
-const FlipCard: React.FC<FlipCardProps> = ({ engineer, delay = 0 }) => {
-  return (
+const FlipCard: React.FC<FlipCardProps> = ({ engineer, delay = 0 }) => {  return (
     <motion.div
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
